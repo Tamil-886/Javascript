cf4b5d080bd90866befa75ef49cb7f29ca4f110d
Tamil <tamild886@gmail.com>
2026-06-28 18:15:18 +0530

    Initial commit task 2

--- Task 1/task1.js
+++
@@ -1,4 +0,0 @@
-// Task 1 - Employee Name
-let employeeName = prompt("Enter employee name:");
-console.log("Employee Name:", employeeName);
-document.writeln("Welcome " + employeeName);
--- Task 2/script.js
+++
@@ -1,3 +0,0 @@
-// Task 2 - Company Entry Confirmation
-let answer = confirm("Are you ready to join today's JavaScript training?");
-console.log(answer);
